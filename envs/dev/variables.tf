f4285bc88212818a923790d7690a9185f6725f98
sergii chreevko <sergii.cherevko.github@gmail.com>
2026-02-09 16:33:08 -0600

    Added subnets. Removed dup var

--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -3,11 +3,6 @@ variable "aws_region" {
   default = "us-east-1"
 }
 
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
-
 variable "vpc_cidr" {
   type    = string
   default = "10.60.0.0/16"
