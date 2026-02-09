787535f26e7c0aa7a98226f31f027542e3b7474e
sergii chreevko <sergii.cherevko.github@gmail.com>
2026-02-09 16:31:26 -0600

    added subnets

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "main" {
-  cidr_block           = "10.60.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
@@ -16,3 +16,28 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_subnet" "public" {
+  count                   = length(var.public_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "dev-public-${var.azs[count.index]}"
+    Tier = "public"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count                   = length(var.private_subnet_cidrs)
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnet_cidrs[count.index]
+  availability_zone       = var.azs[count.index]
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name = "dev-private-${var.azs[count.index]}"
+    Tier = "private"
+  }
+}
--- envs/dev/variables.tf
+++ envs/dev/variables.tf
@@ -2,3 +2,28 @@ variable "aws_region" {
   type    = string
   default = "us-east-1"
 }
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "10.60.0.0/16"
+}
+
+variable "azs" {
+  type    = list(string)
+  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+}
+
+variable "public_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.60.0.0/20", "10.60.16.0/20", "10.60.32.0/20"]
+}
+
+variable "private_subnet_cidrs" {
+  type    = list(string)
+  default = ["10.60.48.0/20", "10.60.64.0/20", "10.60.80.0/20"]
+}
